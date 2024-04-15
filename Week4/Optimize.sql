b59083e43c446b7ae6cb0426f3dfbbf68ad60ce4
ThaiDH <thai06102001@gmail.com>
2024-04-15 09:28:55 +0700

    update

--- Week4/Optimize.sql
+++ Week4/Optimize.sql
@@ -4,12 +4,13 @@ SHOW WARNINGS;
 EXPLAIN SELECT * FROM information WHERE age > 25;
 
 EXPLAIN SELECT name FROM information WHERE age > 25;
-CREATE INDEX idx_age ON information(age);
-DROP INDEX idx_age ON information;
+
+-- CREATE INDEX idx_age ON information(age);
+-- DROP INDEX idx_age ON information;
 
 -- Câu 2:
-CREATE INDEX idx_name ON product(name);
-drop INDEX idx_name ON product;
+-- CREATE INDEX idx_name ON product(name);
+-- drop INDEX idx_name ON product;
 
 EXPLAIN SELECT customer_name
 FROM `orders`
@@ -35,7 +36,7 @@ SELECT *
 FROM `orders`
 WHERE order_time < '2025-03-27';
 
-explain 
+EXPLAIN 
 SELECT *
 FROM `orders`
 WHERE YEAR(order_time) < 2025;
@@ -45,7 +46,24 @@ EXPLAIN SELECT * FROM product WHERE name LIKE 'Milk%';
 
 EXPLAIN SELECT id_product FROM product WHERE name = 'Milk';
 
--- Câu 5:
+-- Câu 5
+
+SELECT * 
+FROM product, orders
+WHERE product.id_product = orders.id_product and order_time > '2024-03-26';
+
+
+-- Câu 6
+INSERT INTO orders (id_order, customer_name, order_time) VALUES (5, 'Truong', '2024-04-15');
+
+INSERT INTO orders (id_order, customer_name, order_time) VALUES ('Truong', '2024-04-15');
+
+-- Câu 7 
+UPDATE product SET name = 'Matcha', price = 100 WHERE product_id = 3;
+
+UPDATE product SET name = 'Matcha', price = 100 WHERE product_id = 3 LIMIT 1;
+
+
 
 
 
